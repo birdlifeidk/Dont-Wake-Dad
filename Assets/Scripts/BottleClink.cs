403d67e6b11fe63d49143525053850a1ccdbe1a3
birdlifeidk <emiskimo@gmail.com>
2023-04-05 00:53:55 -0400

    Bottle Test
    I added a vodka bottle to the scene along with a script. It plays a sound upon collision with the player. The volume changes depending on the player's velocity as well.

---
+++ Assets/Scripts/BottleClink.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleClink : MonoBehaviour
+{
+    //Will add the OnCollision actions with the different objects
+    //This will also need to interact with the Winston script. For example, if Johnny's moves exceed a certain volume, it will trigger Winston's character script and wake him up. 
+    //Also add the grab functions so that he can interact with the world. 
+    public Rigidbody johny;
+    public GameObject beerBottle;
+    public AudioSource clink; 
+
+    private void Start()
+    {
+        johny = GetComponent<Rigidbody>(); 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            clink.Play();
+        }
+
+        if (collision.relativeVelocity.magnitude > 1)
+        {
+            clink.volume = .89f; 
+        }
+    }
+}
