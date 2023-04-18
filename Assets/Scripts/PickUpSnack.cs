3f82d27b3f6a76bdf8b9535575601675b9c09a74
birdlifeidk <emiskimo@gmail.com>
2023-04-18 00:21:34 -0400

    Successfully created the pick up script
    Slaaaayyyy

--- Assets/Scripts/PickUpSnack.cs
+++ Assets/Scripts/PickUpSnack.cs
@@ -1,26 +1,49 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
+using Object = UnityEngine.Object;
+
 
 public class PickUpSnack : MonoBehaviour
-{
+{ 
+    [SerializeField] private LayerMask PickUpLayer; 
+    [SerializeField] private Camera PlayerCamera; 
+    [SerializeField] private float PickUpRange;
+    [SerializeField] private Transform Hand; 
+    
+    private Rigidbody chipRigidbody;
+    private Collider chipCollider; 
 
-    public GameObject SnacksEquipped; 
-    void Start()
+    void Update()
     {
-        SnacksEquipped.SetActive(false);
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if (Input.GetKey(KeyCode.E))
+            Ray PickUpRay = new Ray(PlayerCamera.transform.position, PlayerCamera.transform.forward);
+
+            if (Physics.Raycast(PickUpRay, out RaycastHit hitInfo, PickUpRange, PickUpLayer))
             {
-                this.gameObject.SetActive(false);
-                SnacksEquipped.SetActive(true);
+                if (chipRigidbody)
+                {
+                
+                }
+                else
+                {
+                    chipRigidbody = hitInfo.rigidbody;
+                    chipCollider = hitInfo.collider;
+
+                    chipRigidbody.isKinematic = true;
+                    chipCollider.enabled = false; 
+                }
             }
         }
+
+        if (chipRigidbody)
+        {
+            chipRigidbody.position = Hand.position;
+            chipRigidbody.rotation = Hand.rotation; 
+        }
+       
     }
 }
